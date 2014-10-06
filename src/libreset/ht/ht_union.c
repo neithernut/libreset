06e72249c0a02d35a463a6669feb3a897485aec6
Julian Ganz <Julian.Ganz@hs-furtwangen.de>
2014-10-06 09:01:12 +0200

    Fix implementation of ht_union()

--- src/libreset/ht/ht_union.c
+++ src/libreset/ht/ht_union.c
@@ -8,11 +8,16 @@ ht_union(
 ) {
     size_t dest_b = ht_nbuckets(dest);
     size_t bucket_ration = ht_nbuckets(src) / dest_b;
-    //!< @todo: implement case where bucket_ratio is 0
 
     // iterate over all the destination buckets
     while (dest_b--) {
         struct avl* avl_dest = &dest->buckets[dest_b].avl;
+
+        // calculate the range of elements going into dest
+        r_hash min_hash = dest_b << (BITCOUNT(min_hash) - dest->sizeexp);
+        r_hash max_hash;
+        max_hash = min_hash +
+                   ((~(r_hash) 0) >> (BITCOUNT(min_hash) - dest->sizeexp));
 
         // iterate over all the source buckets for a destination
         size_t b = bucket_ration;
@@ -21,7 +26,7 @@ ht_union(
 
             // perform the union merge for the bucket
             struct avl* avl_src = &src->buckets[off + b].avl;
-            int retval = avl_union(avl_dest, avl_src, cfg);
+            int retval = avl_union(avl_dest, avl_src, min_hash, max_hash, cfg);
             if (retval < 0) {
                 return retval;
             }
